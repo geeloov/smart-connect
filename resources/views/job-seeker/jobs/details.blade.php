1089b61d9026ac97b6fb56b0988a6fe9c5a98745
geeloov <119597369+geeloov@users.noreply.github.com>
2025-03-21 17:56:02 +0100

    Delete resources/views/job-seeker/jobs directory

--- resources/views/job-seeker/jobs/details.blade.php
+++
@@ -1,7 +0,0 @@
-@if(!$hasApplied)
-    <a href="{{ route('job-seeker.applications.create', $jobPosition) }}" class="btn btn-primary">
-        Apply Now
-    </a>
-@else
-    <button class="btn btn-secondary" disabled>Already Applied</button>
-@endif 
